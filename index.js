4bd41fc18293ae67261e2fa6bd7d2f87ebc303ae
Mattiwos B <mattiwosbelachew@gmail.com>
2020-01-26 23:21:52 -0800

    moving server files to github for esay edits

---
+++ index.js
@@ -0,0 +1,35 @@
+const express  = require('express')
+const port = process.env.PORT || 3000
+
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
+//required folders
+
+app.use(express.static('public'));
+app.use(express.static('views'));
+app.use(express.static('assets'));
+
+//renders index.html
+app.get('/', function(req, res){
+  res.render( 'index.html');
+});
+
+//socket
+io.on('connection', (socket)=>{
+	
+	socket.on('disconnect', (arg)=>{
+
+	
+	})
+
+
+});
+
+
+server.listen(port, () => {
+    
+    console.log(`Example app listening on port ${port}!`)
+    
+})
+
---
+++ public/sketch.js
@@ -0,0 +1,10 @@
+const socket = io('http://mattiwos.com/');
+
+var x = 0 
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(0);
+}
