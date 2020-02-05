c4d1758b4d9ecd6be42637845e78919c62a6045c
Mattiwos Belachew <mattiwosbelachew@Mattiwoss-MacBook-Pro-443.local>
2020-02-05 11:53:48 -0800

    listen

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = require('express')();
 var http = require('http').createServer(app);
-var io = require('socket.io')(http);
+var io = require('socket.io').listen(http);
 const port = process.env.PORT || 5500;
 
 //required folders
