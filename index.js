5323ee6a4614b3699f2d262a597712ba8db7b57d
Mattiwos B <mattiwosbelachew@gmail.com>
2020-02-04 17:38:05 -0800

    test

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
-const express  = require('express');
-var app = express();
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
-
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 var port = process.env.PORT || 5500;
 
 //required folders
