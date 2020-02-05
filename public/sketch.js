aa06e8cb0f0b94dbdb3e381c877c9c72325b010b
Mattiwos B <mattiwosbelachew@gmail.com>
2020-02-04 19:32:24 -0800

    test

--- public/sketch.js
+++ public/sketch.js
@@ -12,7 +12,7 @@
 
 // var socket = io.connect(window.location.origin); //starts connection with website (server)
 //const socket = io(window.location.origin);
-var socket = io.connect('http://mattiwos.com/');
+var socket = io();
 //var socket = io();
 
 socket.on("updateLoc", (args)=>{
