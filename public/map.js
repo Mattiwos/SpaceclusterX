52f1ec7e3eb1cf1498769c0bb06b01f291abb70d
Mattiwos Belachew <mattiwosbelachew@lich.local>
2020-03-05 11:53:37 -0800

    just testing it

--- public/map.js
+++ public/map.js
@@ -4,7 +4,7 @@ class Map{
         this.mx=width-80;
         this.my=height-80;
 
-        this.distanceconstant=0.05;
+        this.distanceconstant=0.5;
         
         fill(255);
 
@@ -13,13 +13,13 @@ class Map{
         for(let i=0;i<oplayers.length;i++){
 
             fill(0);
-            this.potentialx= oplayers[i].x- player.x + width/2 ;
+            this.potentialx= oplayers[i].x - player.x + width/2 ;
 
             this.shiftx = width/2 - (this.potentialx);
 
-            this.potentialx+=this.shiftx *this.distanceconstant;
+            this.potentialx+= this.shiftx *this.distanceconstant;
 
-            this.potentialy= oplayers[i].y- player.y + height/2 + m;
+            this.potentialy= oplayers[i].y - player.y + height/2 + m;
 
             this.shifty = height/2 + m - (this.potentialy);
 
@@ -27,7 +27,19 @@ class Map{
 
             print(this.potentialx+" "+ this.potentialy);
             
-            ellipse( this.potentialx + (this.mx-width/2), this.potentialy + (this.my - (height/2+m) ), 10, 10);
+            ellipse((this.mx-width/2) + this.potentialx , this.potentialy + (this.my - (height/2+m) ), 10, 10);
+            
+        }
+        //using this to test it
+
+        noStroke();
+        fill(255);
+        ellipse(this.mx,this.my -100,100,100);
+        for(let i=0;i<oplayers.length;i++){
+
+            fill (0);
+            ellipse (this.mx -10,this.my-100, 10,10)
+
             
         }
 
