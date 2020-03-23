1492cf999f9b989cd360ff3fc39c3a38a2016a8d
Sam Reifenstein <samreifenstein@EDWARDs-MacBook-Pro-2.local>
2020-03-23 12:41:11 -0700

    dixed a resource froblem

--- public/hub.js
+++ public/hub.js
@@ -144,7 +144,7 @@ class hub {
 
         this.hotkey=[49,50,51];
 
-        for(let i=0;i<numOfResources;i++)this.availableExports.push(i+1);
+        for(let i=1;i<numOfResources;i++)this.availableExports.push(i);
 
         
         //alert(this.availableExports);
